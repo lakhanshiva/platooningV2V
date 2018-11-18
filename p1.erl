2345b1b96e348d336489bb59c297e985dc848142
Lakhan Kamireddy <lakhanshiva@gmail.com>
2018-11-17 18:14:40 -0700

    Edit leader process

--- p1.erl
+++ p1.erl
@@ -1,18 +1,18 @@
+%% 11/17/18
+%% Lakhan Kamireddy
+
 -module(p1).
 
 -export([start/0, leader/0]).
 
-leader() ->
+leader() -> %% This is the leader process function
 	receive
 		finished ->
 			io:format("Leader finished~n", []);
-		{drive, Drive_PID} ->
-			io:format("Leader received the command to drive forward~n", []),
-			Drive_PID ! leader,
-			leader()
+		drive ->
+			io:format("Leader received the command to drive forward~n", [])
 	end.
 
-start() ->
-	Leader_PID = spawn(p1, leader, []),
-	spawn(p1, drive, [0, Leader_PID]),
-	Leader_PID ! {drive, 1}.
+start() -> %% program execution begins here
+	Leader_PID = spawn(p1, leader, []), %% Spawning the leader process
+	Leader_PID ! drive. %% Sending the command drive to the leader process
